0a78a5ca5c6e755f03b9e45f02631e7e55d79075
jonaspereira <jonas.pereira@iwantgreatcare.org>
2016-05-10 17:55:15 +0100

    add metadata.rb

---
+++ cookbooks/main/metadata.rb
@@ -0,0 +1,8 @@
+name             'main'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures main'
+long_description 'Installs/Configures main'
+version          '0.1.0'
+depends 'users'
